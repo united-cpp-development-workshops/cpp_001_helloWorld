33667b6fee74e9f00c7e8430b8b2bb87b34858c1
EmrecanKaracayir <developer@karacayir.com>
2023-10-28 18:41:32 +0300

    Initial Commit

---
+++ cpp_001_helloWorld/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_001_helloWorld
+//
+//  Created by Emrecan Karaçayır on 28/10/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
