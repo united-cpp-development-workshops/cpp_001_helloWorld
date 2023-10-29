daed642b998c612cfc323b4f23de288dbf353b9c
EmrecanKaracayir <developer@karacayir.com>
2023-10-29 09:29:17 +0300

    init

--- cpp_001_helloWorld/main.cpp
+++ cpp_001_helloWorld/main.cpp
@@ -1,14 +1,7 @@
-//
-//  main.cpp
-//  cpp_001_helloWorld
-//
-//  Created by Emrecan Karaçayır on 28/10/23.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-  // insert code here...
-  std::cout << "Hello, World!\n";
+int main()
+{
+  std::cout << "Hello, world!" << std::endl;
   return 0;
 }
